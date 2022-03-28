e69324db1e289f8d5d99193ea8606db3092502be
Ramya <Ramya@LAPTOP-V2H6MTKJ>
2022-03-28 13:34:51 +0530

    created employee

---
+++ src/test/java/com/crm/autodesk/Practice/CreateEmpTest.java
@@ -0,0 +1,12 @@
+package com.crm.autodesk.Practice;
+
+import org.testng.annotations.Test;
+
+public class CreateEmpTest {
+	@Test
+	public void createNewEmp()
+	{
+		System.out.println("created new employee");
+	}
+
+}
